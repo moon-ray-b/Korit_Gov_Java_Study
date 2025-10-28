5bae7b56787bb602f5185ddbf39c1bba1426b55f
bae9842 <bae9842@naver.com>
2025-10-28 16:39:01 +0900

    Feat: Static

---
+++ src/_23_Static/Person/Main.java
@@ -0,0 +1,12 @@
+package _23_Static.Person;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person();
+        Person.showPopulation();
+        Person person2 = new Person();
+        Person.showPopulation();
+        Person person3 = new Person();
+        Person.showPopulation();
+    }
+}
